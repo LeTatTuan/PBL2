ff3aaf885d318afa7881824246fbd422de7d1167
LaDangHuy <lahuy136@gmail.com>
2023-12-26 11:39:09 +0700

    clean code for the first time

--- src/main/java/idm/handleEvent/UpdateUIEverySecond.java
+++ src/main/java/idm/handleEvent/UpdateUIEverySecond.java
@@ -6,43 +6,32 @@ import idm.models.DownloadInfo;
 import javafx.scene.control.TextArea;
 
 public class UpdateIntefaceEvery1s implements Runnable {
-    private DownloadInfo downloadInfo;
+    private final DownloadInfo downloadInfo;
     private long downloadedBefore1s;
     private volatile AtomicBoolean pause = new AtomicBoolean(false);
     private volatile AtomicBoolean cancel = new AtomicBoolean(false);
-
-
 
     public UpdateIntefaceEvery1s(DownloadInfo downloadInfo, long downloadedBefore1s, AtomicBoolean pause, AtomicBoolean cancel) {
      this.downloadInfo = downloadInfo;
      this.downloadedBefore1s = downloadedBefore1s;
      this.pause = pause;
      this.cancel = cancel;
-     
     }
-    
 
     @Override
     public void run() {
         if (cancel.get()) {
             return;
         }
-
         if (!pause.get()) {
             long downloadedSize = downloadInfo.getDownloaded();
             long size = downloadInfo.getSize();
-            double transferRate;
-            double timeLeft = 0;
-
-            //bytes - bytes
-            transferRate = (double) (downloadedSize - downloadedBefore1s) / 1000;
+            double transferRate = (double) (downloadedSize - downloadedBefore1s) / 1000;
             double transferRateMB = transferRate / 1024;
-            timeLeft = (size - downloadedSize) / (transferRate * 1024);
+            double timeLeft = (size - downloadedSize) / (transferRate * 1024);
             downloadInfo.setTransferRate(transferRateMB);
-            downloadInfo.setTimeleft(timeLeft);
+            downloadInfo.setTimeLeft(timeLeft);
             this.downloadedBefore1s = downloadedSize;
-        }
-        else {
         }
     }
 
